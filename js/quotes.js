e98e1b171fee98f1bebeba42d06d3d5434d01a68
TheBinaryDigit <hi.binarydigit@gmail.com>
2022-06-03 22:09:58 -0400

    done with quote feature

--- js/quotes.js
+++ js/quotes.js
@@ -1,15 +1,4 @@
-function inIframe() {
-    try {
-        return window.self !== window.top;
-    } catch (e) {
-        return true;
-    }
-}
-		var colors = [
-            'white',
-            'grey'
-		];
-        
+   
 	var quotes = [
 		["I'm not chewing it I'm just sucking it in my mouth. -KilobyteKeith Dead by Daylight 02/20/2020"], 
 		["Just because you mate doesn't mean you are happy. -KilobyteKeith Planet Zoo 03/15/2020"], 
@@ -348,47 +337,17 @@ function inIframe() {
 var currentQuote = "";
 var currentAuthor = "";
 var randomquote = "";
-var randomcolor = "";
 
 function getQuote() {
 	randomquote = Math.floor(Math.random() * quotes.length);
-	randomcolor = Math.floor(Math.random() * colors.length);
     currentQuote = quotes[randomquote][0];
     currentAuthor = quotes[randomquote][1];
-	if (inIframe()) {
-		$('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=aLamm&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
-	}
 
 	$(document).ready(function () {
-	    $('html body').animate({
-	        backgroundColor: colors[randomcolor],
-	        color: colors[randomcolor]
-	    }, 500);
-	    $('#newquote').animate({ backgroundColor: colors[randomcolor] }, 500);
-			$('blockquote footer').animate({ color: colors[randomcolor] }, 500);	
-	    $('blockquote').animate({ borderLeftColor: colors[randomcolor] }, 500);
-	    $('#quotetext').animate({ opacity: 0 }, 500, function () {
-	        $(this).animate({ opacity: 1 }, 500);
-	        $(this).text(currentQuote);
-	    });
-	    $('#quotesource').animate({ opacity: 0 }, 500, function () {
-	        $(this).animate({ opacity: 1 }, 500);
-	        $(this).text(currentAuthor);
-	    });
-    });    
-}
-
-function openURL(url) {
-    window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
+	    $('#quotetext').text(currentQuote);
+		$('#newquote').on('click', getQuote);
+	});
+    
 }
 
 getQuote();
-
-$(document).ready(function () {
-    $('#newquote').on('click', getQuote);
-    $('#tweetquote').on('click', function () {
-        if (!inIframe()) {
-            openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
-        }
-    });
-});
